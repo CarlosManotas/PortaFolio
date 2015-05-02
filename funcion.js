d51f16c2de6ef178ad3ede1b2323253700a378f1
Carlos Manotas <carlosmanotas@hotmail.com>
2015-05-02 09:23:36 -0300

    seccion ilustraciones listo con responsive

--- funcion.js
+++ funcion.js
@@ -2,4 +2,24 @@ $(function() {
 	$('#mostrar-menu').on('click', function(){
 		$(this).next().slideToggle();
 	})
+
+	$('#container img').on('click', abrir);
+	$('#imgFull').on('click', cerrar);
+
+
 });
+
+function abrir(){
+	//alert($(this).attr('alt'));
+
+	var nombre = $(this).attr('alt');
+	var direccion = "imagenes/" + nombre + ".jpg";
+
+	$('#imgFull').attr('src', direccion);
+
+	$('#previa').fadeIn();
+}
+
+function cerrar(){
+	$('#previa').fadeOut();
+}
